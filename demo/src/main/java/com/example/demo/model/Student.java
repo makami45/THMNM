bdea220b0dc3f36d3c99834d6d29a9ff8d7735f0
xuannhat1503 <nguyenxuannhat15324@gmail.com>
2025-12-19 00:37:53 +0700

    Model: use Long id to accept null from JSON

--- demo/src/main/java/com/example/demo/model/Student.java
+++ demo/src/main/java/com/example/demo/model/Student.java
@@ -8,22 +8,22 @@ import jakarta.persistence.Id;
 @Entity
 public class Student {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	private String name;
 	private String email;
 	public Student() {
 		super();
 	}
-	public Student(long id, String name, String email) {
+	public Student(Long id, String name, String email) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
 	}
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 	public String getName() {
