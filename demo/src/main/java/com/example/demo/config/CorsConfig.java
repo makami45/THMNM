ef7ada1619a3efe0e54ad281dcace05ee4d93025
xuannhat1503 <nguyenxuannhat15324@gmail.com>
2025-12-19 00:10:33 +0700

    Cors: allow all origins, disable credentials

--- demo/src/main/java/com/example/demo/config/CorsConfig.java
+++ demo/src/main/java/com/example/demo/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig implements WebMvcConfigurer {
             .allowedOriginPatterns("*")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
-            .allowCredentials(true)
+            .allowCredentials(false)
             .maxAge(3600);
     }
 }
