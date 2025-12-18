c70ebce1ef6ba0c3a200b3250fd873e9ecfa14dc
xuannhat1503 <nguyenxuannhat15324@gmail.com>
2025-12-18 17:58:41 +0700

    tao du an

---
+++ demo/src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import com.example.demo.repoistory.StudentRepository;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.*;
+
+@RestController
+@RequestMapping("/api/students")
+@CrossOrigin(origins = "http://localhost:3000")
+public class StudentController {
+	private final StudentRepository repository;
+
+	public StudentController(StudentRepository repository) {
+		this.repository = repository;
+	}
+
+	@GetMapping
+	public List<Student> getAllStudents() {
+		return repository.findAll();
+	}
+}
